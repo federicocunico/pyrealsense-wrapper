f9ae62a4d47b3ff815f993acd348b3b4043cc439
elos94 <fede1515>
2020-03-30 00:12:27 +0200

    .

--- record.py
+++ record.py
@@ -2,8 +2,10 @@ import os
 import shutil
 import cv2
 from PIL import Image
+import imageio
 from lib.realsense import RealSense
-from ffmpeg
+
+# import ffmpeg
 
 # Not working -> non readable
 # def write_video(out_file, frames, fps=30, size=(640, 480)):
@@ -14,25 +16,34 @@ from ffmpeg
 #     out.release()
 
 
-def write_video(fn, images, framerate=30, vcodec='libx264'):
-    if not isinstance(images, np.ndarray):
-        images = np.asarray(images)
-    n, height, width, channels = images.shape
-    process = (
-        ffmpeg
-        .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
-        .output(fn, pix_fmt='yuv420p', vcodec=vcodec, r=framerate)
-        .overwrite_output()
-        .run_async(pipe_stdin=True)
-    )
-    for frame in images:
-        process.stdin.write(
-            frame
-            .astype(np.uint8)
-            .tobytes()
-        )
-    process.stdin.close()
-    process.wait()
+# def write_video(fn, images, framerate=30, vcodec='libx264'):
+# if not isinstance(images, np.ndarray):
+#     images = np.asarray(images)
+# n, height, width, channels = images.shape
+# process = (
+#     ffmpeg
+#     .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
+#     .output(fn, pix_fmt='yuv420p', vcodec=vcodec, r=framerate)
+#     .overwrite_output()
+#     .run_async(pipe_stdin=True)
+# )
+# for frame in images:
+#     process.stdin.write(
+#         frame
+#         .astype(np.uint8)
+#         .tobytes()
+#     )
+# process.stdin.close()
+# process.wait()
+
+
+def write_video(fn, images, fps=30, format='mp4'):
+    writer = imageio.get_writer(fn, format=format, mode='I', fps=fps)
+    for i in range(len(images)):
+        frame = images[i]
+
+        writer.append_data(frame)
+    writer.close()
 
 
 def save_jpgs():
@@ -56,6 +67,7 @@ def run(resolution):
         cv2.imshow('rgb', rgb)
         if cv2.waitKey(1) == ord('q'):
             break
+        rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
         # frames.append(rgb.copy())  # N.B. copy
         frames.append(rgb)
 
@@ -69,10 +81,9 @@ def run(resolution):
 
     # ffmpeg
     write_video(os.path.join(cwd, 'output.mp4'), frames)
-    
+
     # opencv
     # write_video(os.path.join(cwd, 'output.mp4'), frames, size=(resolution[1], resolution[1]))
-    
 
 
 if __name__ == "__main__":
